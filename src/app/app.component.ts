ed47933b209eb315cee5ad03285a5ed11d04db1d
Ken Larson <servantfist@users.noreply.github.com>
2022-11-24 00:16:16 -0700

    updates after stackblitz download

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, VERSION } from '@angular/core';
 import { of, from, map, pipe, tap, catchError } from 'rxjs';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
